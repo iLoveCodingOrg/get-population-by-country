6fc23a94737c3b0ac6d4cf214943f0df74d86314
Noor Syed <noorsyed23@gmail.com>
2019-06-03 16:20:48 -0500

    step 1

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-// get the countries from API
-// print these countries
-  // create a element for each country
-  // add that element to the select drop down
+// When the Select OPTION changes
+// Make another call to get the info
+// Print the info on the screen
 
-  document.addEventListener('DOMContentLoaded', ()=>{
-    const selectElem = document.querySelector('select#countries')
-    fetch('https://dacmzozdwasla.cloudfront.net/1.0/countries', {
+document.addEventListener('DOMContentLoaded', ()=>{
+    const selectElem = document.querySelector('select#countries');
+  
+    selectElem.addEventListener('change', (event)=>{
+      console.log(event.target.selectedOptions[0].innerText)
+    })
+  
+  
+    fetch('http://api.population.io:80/1.0/countries', {
       headers: {
         accept: 'application/json; charset=utf=8'
       }

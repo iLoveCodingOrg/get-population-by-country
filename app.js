4cffd537971ad5440ea7a1799ee3d890fd82dedb
Noor Syed <noorsyed23@gmail.com>
2019-06-03 16:27:49 -0500

    step 2

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 // Print the info on the screen
 
 document.addEventListener('DOMContentLoaded', ()=>{
+    const population = document.querySelector('.population')
     const selectElem = document.querySelector('select#countries');
   
     selectElem.addEventListener('change', (event)=>{
-      console.log(event.target.selectedOptions[0].innerText)
+      getPopulation(event.target.selectedOptions[0].innerText);
     })
-  
   
     fetch('http://api.population.io:80/1.0/countries', {
       headers: {
@@ -28,6 +28,21 @@ document.addEventListener('DOMContentLoaded', ()=>{
         const elem = document.createElement('option')
         elem.textContent = item;
         selectElem.appendChild(elem)
+      })
+    }
+  
+    function getPopulation(country){
+      fetch(`http://api.population.io:80/1.0/population/2000/${country}/100/`, {
+        headers: {
+          accept: 'application/json; charset=utf=8'
+        }
+      })
+      .then((res)=>{
+        return res.json()
+      })
+      .then((json)=>{
+        population.textContent = json[0].total
+        console.log(json)
       })
     }
   

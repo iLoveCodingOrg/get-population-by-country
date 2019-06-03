146cbd2b809297115767a0ef87f45dc9e895e38f
Noor Syed <noorsyed23@gmail.com>
2019-06-03 16:13:55 -0500

    list of countries

---
+++ app.js
@@ -0,0 +1,29 @@
+// get the countries from API
+// print these countries
+  // create a element for each country
+  // add that element to the select drop down
+
+  document.addEventListener('DOMContentLoaded', ()=>{
+    const selectElem = document.querySelector('select#countries')
+    fetch('https://dacmzozdwasla.cloudfront.net/1.0/countries', {
+      headers: {
+        accept: 'application/json; charset=utf=8'
+      }
+    })
+      .then((res)=>{
+        return res.json()
+      })
+      .then((json)=>{
+        printCountries(json.countries)
+      })
+  
+    function printCountries(list){
+      console.log(list)
+      list.forEach((item)=>{
+        const elem = document.createElement('option')
+        elem.textContent = item;
+        selectElem.appendChild(elem)
+      })
+    }
+  
+  })
